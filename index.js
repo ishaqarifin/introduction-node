d5457be070856e0abf50e1a661c3080af386c0df
ishaqarifin <ishaqarifin071@gmail.com>
2022-02-11 12:48:22 +0700

    day 3 manipulation object

--- index.js
+++ index.js
@@ -1,16 +1,115 @@
-// memanggil package express
-const express = require("express"); //import express from 'express'
+// Pemanggilan package express
+const express = require("express");
 
-//menggunakan package express
-const app = express(); //app=aplikasi
+// Menggunakan package express
+const app = express();
 
-//set endpoint
-app.get("/", function (req, respon) {
-  respon.send("hello world");
+// set template engine
+app.set("view engine", "hbs");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+
+// true => sudah login
+// false => belum login
+const isLogin = true;
+
+const blogs = [
+  {
+    title: "Pasar Coding di Indonesia Dinilai Masih Menjanjikan",
+    content:
+      "Ketimpangan sumber daya manusia (SDM) di sektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan penelitian ManpowerGroup, ketimpangan SDM global, termasuk Indonesia, meningkat dua kali lipat dalam satu dekade terakhir. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, molestiae numquam! Deleniti maiores expedita eaque deserunt quaerat! Dicta, eligendi debitis?",
+    author: "Ichsan Emrald Alamsyah",
+    posted_at: "12 Jul 2021 22:30 WIB",
+  },
+];
+
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// Set endpoint
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
 
-//konfigurasi port aplikasi
+app.get("/home", function (req, res) {
+  res.render("index");
+});
+
+app.get("/blog", function (req, res) {
+  console.log(blogs); // hanya ada 4 properti
+
+  let dataBlogs = blogs.map(function (data) {
+    return {
+      ...data,
+      isLogin: isLogin,
+    };
+  });
+
+  res.render("blog", { isLogin: isLogin, blogs: dataBlogs });
+});
+
+app.get("/add-blog", function (req, res) {
+  if (!isLogin) {
+    res.redirect("/home");
+  }
+
+  res.render("form-blog");
+});
+
+app.post("/blog", function (req, res) {
+  let title = req.body.title;
+  let content = req.body.content;
+  let date = new Date();
+
+  let blog = {
+    title: title,
+    content,
+    author: "Ichsan Emrald Alamsyah",
+    posted_at: getFullTime(date),
+  };
+
+  blogs.push(blog);
+
+  res.redirect("/blog");
+});
+
+app.get("/blog/:id", function (req, res) {
+  let id = req.params.id;
+  console.log(`Id dari client : ${id}`);
+
+  res.render("blog-detail", { id: id });
+});
+
+app.get("/delete-blog/:index", function (req, res) {
+  let index = req.params.index;
+
+  console.log(`Index data : ${index}`);
+
+  blogs.splice(index, 1);
+  res.redirect("/blog");
+});
+
+app.get("/contact-me", function (req, res) {
+  res.render("contact");
+});
+
+// Konfigurasi port aplikasi
 const port = 5000;
 app.listen(port, function () {
-  console.log(`server running on ${port}`);
+  console.log(`Server running on port ${port}`);
 });
+
+function getFullTime(time) {
+  let date = time.getDate();
+  let monthIndex = time.getMonth();
+  let year = time.getFullYear();
+
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`;
+}
